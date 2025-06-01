431575ad1751fce95984becae628c288507f3099
David Freitas <davidfreitas@proton.me>
2025-06-01 01:10:24 -0300

    Header and footer basic styling

--- Client/src/app/components/footer/footer.component.ts
+++ Client/src/app/components/footer/footer.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatDividerModule,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
--- Client/src/app/components/header/header.component.ts
+++ Client/src/app/components/header/header.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatDividerModule,
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  
 }
