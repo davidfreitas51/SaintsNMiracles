358d0b5f23be43158bded99aa6386d7349e3152e
David Freitas <davidfreitas@proton.me>
2025-06-01 23:39:13 -0300

    Saints demo

--- Client/src/app/pages/saints-details-page/saints-details-page.component.ts
+++ Client/src/app/pages/saints-details-page/saints-details-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../components/footer/footer.component";
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-saints-details-page',
-  imports: [],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './saints-details-page.component.html',
   styleUrl: './saints-details-page.component.scss'
 })
