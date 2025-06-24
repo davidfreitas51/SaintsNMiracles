76afe526a9872e1f756078b40303568a23679cb8
David Freitas <davidfreitas51@proton.me>
2025-06-24 11:54:40 -0300

    Basic structure for saint details page

--- Client/src/app/pages/saint-details-page/saint-details-page.component.ts
+++ Client/src/app/pages/saint-details-page/saint-details-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { SaintsService } from '../../services/saints.service';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-saint-details-page',
@@ -13,18 +14,24 @@ export class SaintDetailsPageComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private saintsService = inject(SaintsService);
 
+  imageBaseUrl = environment.assetsUrl;
+  public saint: any = null;
+
   ngOnInit(): void {
-    const fullPath = this.route.snapshot.url
-      .map((segment) => segment.path)
-      .join('/');
-    const slug = fullPath.split('/').pop() || '';
+    const slug =
+      this.route.snapshot.url
+        .map((s) => s.path)
+        .join('/')
+        .split('/')
+        .pop() || '';
 
     this.saintsService.getSaint(slug).subscribe({
       next: (saint) => {
-        console.log('Saint:', saint);
+        this.saint = saint;
+        console.log(saint);
       },
       error: (err) => {
-        console.error('Erro ao buscar santo:', err);
+        console.error('Erro ao carregar santo:', err);
       },
     });
   }
